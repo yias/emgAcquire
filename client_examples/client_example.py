4d760c065c5a2fa464b7c490ae87a66c468e02a8
yias <iasonbatz@gmail.com>
2020-06-03 10:24:33 +0200

    modifications on the python client example

--- client_examples/client_example.py
+++ client_examples/client_example.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 
-import emgAcquireClient
+import os
+import sys
+import pathlib
 import numpy as np
+
+
+# directory of the emgAcquireClient python_module 
+emgAquire_dir = str(pathlib.Path().absolute()) + "\\..\\python_module"
+print(emgAquire_dir)
+
+# append the path including the directory of the python_module
+sys.path.append(emgAquire_dir)
+
+# import the module
+import emgAcquireClient
+
 
 
 emgClient = emgAcquireClient.emgAcquireClient()
