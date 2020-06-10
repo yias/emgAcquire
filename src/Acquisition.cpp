8607a440f54c99d80042adbe1b3006c6947bbb5f
yias <iasonbatz@gmail.com>
2020-06-10 20:59:49 +0200

    bringing back the original acquisition frequency

--- src/Acquisition.cpp
+++ src/Acquisition.cpp
@@ -498,7 +498,7 @@ int Acquisition::continuousUpdate(){
         if(!isSetNew){
             isSetNew = true;
         }
-        std::this_thread::sleep_for(std::chrono::duration<int, std::milli>(int( (1000/sampling_frequency) - 8) ));
+        std::this_thread::sleep_for(std::chrono::duration<int, std::milli>(int( (1000/sampling_frequency) ) ));
     }
 
     std::cout << "[Acquisition] Stopped acquiring signals continuously" << std::endl;
